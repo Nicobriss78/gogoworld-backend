63c2890b30fcfb9efeafa58f6df294c6aa53bbc0
Nicobriss78 <nicobriss@yahoo.com>
2026-04-18 22:31:52 +0200

    Create checkInRoutes.js

---
+++ routes/checkInRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const { protect } = require("../middleware/auth");
+const { checkInLimiter } = require("../middleware/rateLimit");
+
+const {
+  createCheckIn,
+  getCheckInStatus,
+  getEventCheckInSummary,
+  listEventCheckIns,
+} = require("../controllers/checkInController");
+
+router.post("/", protect, checkInLimiter, createCheckIn);
+
+router.get("/events/:id/status", protect, getCheckInStatus);
+router.get("/events/:id/summary", protect, getEventCheckInSummary);
+router.get("/events/:id/list", protect, listEventCheckIns);
+
+module.exports = router;
