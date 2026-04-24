3492c5084d9f039cf12f3d0bbb672c52dfb6dbea
Nicobriss78 <nicobriss@yahoo.com>
2026-04-24 17:56:03 +0200

    Update checkInController.js

--- controllers/checkInController.js
+++ controllers/checkInController.js
@@ -210,7 +210,12 @@ try {
 });
 
 const getCheckInStatus = asyncHandler(async (req, res) => {
-  const eventId = req.params.id;
+  const eventId = normalizeEventId(req.params.id);
+
+if (!eventId) {
+  res.status(400);
+  throw new Error(CHECKIN_REASON.INVALID_EVENT_ID);
+}
   const userId = req.user?._id;
 
   const event = await Event.findById(eventId)
