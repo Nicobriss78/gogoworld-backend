80d109fda67c6d6e0fef6bffab2a1585f3d1a006
Nicobriss78 <nicobriss@yahoo.com>
2026-05-09 06:30:51 +0200

    Update geocodeController.js

--- controllers/geocodeController.js
+++ controllers/geocodeController.js
@@ -1,7 +1,12 @@
 const asyncHandler = require("express-async-handler");
-const { geocodeAddress } = require("../services/geocodeService");
+const { geocodeAddress, reverseGeocode } = require("../services/geocodeService");
 
 exports.searchGeocode = asyncHandler(async (req, res) => {
   const result = await geocodeAddress(req.body || {});
+  res.json(result);
+});
+
+exports.reverseGeocode = asyncHandler(async (req, res) => {
+  const result = await reverseGeocode(req.body || {});
   res.json(result);
 });
