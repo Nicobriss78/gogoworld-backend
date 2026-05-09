8cdd165135474dd277f9b6f946d149be0e58100b
Nicobriss78 <nicobriss@yahoo.com>
2026-05-09 06:31:52 +0200

    Update geocodeRoutes.js

--- routes/geocodeRoutes.js
+++ routes/geocodeRoutes.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 const { writeLimiter } = require("../middleware/rateLimit");
 const { securityRateLimit } = require("../middleware/securityRateLimit");
-const { searchGeocode } = require("../controllers/geocodeController");
+const { searchGeocode, reverseGeocode } = require("../controllers/geocodeController");
 
 const geocodeLimiter = securityRateLimit({
   scope: "geocode_search",
+  windowMs: 60_000,
+  max: 10,
+});
+
+const reverseGeocodeLimiter = securityRateLimit({
+  scope: "geocode_reverse",
   windowMs: 60_000,
   max: 10,
 });
@@ -19,6 +25,15 @@ router.post(
   geocodeLimiter,
   authorize("organizer"),
   searchGeocode
+);
+
+router.post(
+  "/reverse",
+  writeLimiter,
+  protect,
+  reverseGeocodeLimiter,
+  authorize("organizer"),
+  reverseGeocode
 );
 
 module.exports = router;
