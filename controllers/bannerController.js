32c35286f880036c5ea25f9f3913f5424e39bf5e
Nicobriss78 <nicobriss@yahoo.com>
2026-05-28 13:46:42 +0200

    Update bannerController.js

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -10,6 +10,9 @@ normalizeGeoTarget,
 const {
 checkPromoAvailability,
 } = require("../services/promoAvailabilityService");
+const {
+  calculateDemandFromAvailability,
+} = require("../services/promoDemandScarcityService");
 // Cache semplice in RAM con TTL per lista attiva e indice round-robin per chiave
 const activeCache = new Map(); // key -> { expiresAt, items: [banner], rr: 0 }
 const TTL_MS = 60 * 1000; // 60s: abbastanza breve per B1/1
