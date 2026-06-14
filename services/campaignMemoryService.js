c7b9b97cc848a11424735323f8349517c745be3a
Nicobriss78 <nicobriss@yahoo.com>
2026-06-14 21:21:01 +0200

    Update campaignMemoryService.js

--- services/campaignMemoryService.js
+++ services/campaignMemoryService.js
@@ -45,7 +45,7 @@ function buildSnapshotPayload({ banner, dailyStats, nowDate }) {
 
   return {
     bannerId: banner._id,
-    snapshotVersion: 2,
+    snapshotVersion: 3,
     eventId: banner.eventId || null,
     organizerId: banner.createdBy || null,
     snapshotStatus: "COMPLETED",
